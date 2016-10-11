9d9081368ea8628c12d582830523fe17ba459a46
SteveK <stevekm@users.noreply.github.com>
2016-10-11 10:33:44 -0400

    update IGV test bat with new format needed

--- code/IGV_test.bat
+++ code/IGV_test.bat
@@ -1,6 +1,7 @@
 new
-snapshotDirectory /ifs/home/kellys04/projects/clinical_genomic_reporting/reporter_files/output/R_2016_07_28_15_04_57_user_SN2-182-IT16-034/IGV_snapshot
-load /ifs/home/kellys04/projects/clinical_genomic_reporting/reporter_files/output/R_2016_07_28_15_04_57_user_SN2-182-IT16-034/bam/IonXpress_002_R_2016_07_28_15_04_57_user_SN2-182-IT16-034_Auto_user_SN2-182-IT16-034_233.bam
+snapshotDirectory /ifs/home/kellys04/projects/clinical_genomic_reporting/reporter_files/output/R_2016_07_28_15_04_57_user_SN2-182-IT16-034/IGV_snapshot2
+load /ifs/home/kellys04/projects/clinical_genomic_reporting/reporter_files/output/R_2016_07_28_15_04_57_user_SN2-182-IT16-034/IonXpress_002/IonXpress_002_R_2016_07_28_15_04_57_user_SN2-182-IT16-034_Auto_user_SN2-182-IT16-034_233.bam
+load /ifs/home/kellys04/projects/clinical_genomic_reporting/reporter_files/output/R_2016_07_28_15_04_57_user_SN2-182-IT16-034/IonXpress_007/IonXpress_007_R_2016_07_28_15_04_57_user_SN2-182-IT16-034_Auto_user_SN2-182-IT16-034_233.bam
 genome hg19
 maxPanelHeight 2000
 goto chr11:108138003-108138003
