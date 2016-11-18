860e000f0bdbdc5f711ac92616451eb1944b4071
SteveK <stevekm@users.noreply.github.com>
2016-11-18 13:14:26 -0500

    update run ID script to search file itself

--- code/get_run_IDs.sh
+++ code/get_run_IDs.sh
@@ -4,9 +4,21 @@
 
 ## DESCRIPTION: This script will read in the run.xls file and output just the sample IDs
 
-run_xls="$1"
+run_dir="$1"
+
+
+# get first match to run XLS
+run_xls="$(find "$run_dir" -type f -path "*variantCaller_out*" -name "*.xls" | head -1)"
 
 outdir="$(dirname $run_xls)"
 outfile="${outdir}/sample_barcode_IDs.tsv"
 
 cat "$run_xls" | cut -f46-48 | uniq > "$outfile" && echo -e "Output saved to:\n$outfile"
+
+
+
+# run_xls="$1"
+# outdir="$(dirname $run_xls)"
+# outfile="${outdir}/sample_barcode_IDs.tsv"
+
+# cat "$run_xls" | cut -f46-48 | uniq > "$outfile" && echo -e "Output saved to:\n$outfile"
