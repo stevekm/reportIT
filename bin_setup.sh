86327d5a9ccaee00e6209c1819c38295bba136f6
Stephen Kelly <stevekm@users.noreply.github.com>
2017-02-02 22:01:40 -0500

    update bin install script

--- bin_setup.sh
+++ bin_setup.sh
@@ -3,19 +3,25 @@
 # download bcftools
 # http://www.htslib.org/download/
 if [ ! -f bcftools-1.3.1.tar.bz2 ]; then wget https://github.com/samtools/bcftools/releases/download/1.3.1/bcftools-1.3.1.tar.bz2 ; fi
-
+tar xvfj bcftools-1.3.1.tar.bz2
+(cd bcftools-1.3.1; make)
+ln -fs bcftools-1.3.1/bcftools
+ln -fs htslib-1.3.1/tabix
 
 # download samtools
 if [ ! -f samtools-1.3.1.tar.bz2 ]; then wget https://github.com/samtools/samtools/releases/download/1.3.1/samtools-1.3.1.tar.bz2 ; fi
-
+tar xvfj samtools-1.3.1.tar.bz2
+(cd samtools-1.3.1; make)
+ln -fs samtools-1.3.1/samtools
 
 # hstlib
-if [ ! -f htslib-1.3.2.tar.bz2 ]; then wget https://github.com/samtools/htslib/releases/download/1.3.2/htslib-1.3.2.tar.bz2 ; fi
-
+if [ ! -f htslib-1.3.1.tar.bz2 ]; then wget https://github.com/samtools/htslib/releases/download/1.3.1/htslib-1.3.1.tar.bz2 ; fi
+tar xvfj htslib-1.3.1.tar.bz2
+(cd htslib-1.3.1; make)
+ln -fs htslib-1.3.1/bgzip
 
 # vcflib for vcf2tsv
 # https://github.com/vcflib/vcflib
-
 if [ ! -d vcflib ]; then
     git clone --recursive https://github.com/ekg/vcflib.git
 fi
@@ -28,15 +34,37 @@ if [ ! -f vcflib/bin/vcf2tsv ]; then
         )
     fi
 fi
+ln -fs vcflib/bin/vcf2tsv
 
 
 # pandoc
 if [ ! -f pandoc-1.13.1.zip ]; then wget https://s3.amazonaws.com/rstudio-buildtools/pandoc-1.13.1.zip; fi
+unzip pandoc-1.13.1.zip
+ln -fs pandoc-1.13.1/linux/rpm/x86_64/pandoc
+
 
 # IGV
 # http://software.broadinstitute.org/software/igv/download
 # if [ ! -f IGV_2.3.88.zip ]; then wget http://data.broadinstitute.org/igv/projects/downloads/IGV_2.3.88.zip ; fi
-wget http://data.broadinstitute.org/igv/projects/downloads/IGV_2.3.88.zip
+if [ ! -f IGV_2.3.81.zip ]; then wget http://data.broadinstitute.org/igv/projects/downloads/IGV_2.3.81.zip; fi
+unzip IGV_2.3.81.zip
+
+# ANNOVAR
+# http://annovar.openbioinformatics.org/en/latest/user-guide/download/
+# currently using: refGene,cosmic68,clinvar_20150629,1000g2015aug_all
+if [ ! -f annovar.latest.tar.gz ]; then
+    wget http://www.openbioinformatics.org/annovar/download/0wgxR2rIVP/annovar.latest.tar.gz
+    tar -zxvf annovar.latest.tar.gz
+fi
+db_dir="annovar/db"
+mkdir -p "$db_dir"
+# install ANNOVAR db's
+annovar/annotate_variation.pl -downdb -buildver hg19 -webfrom annovar 1000g2015aug "$db_dir"
+annovar/annotate_variation.pl -downdb -buildver hg19 -webfrom annovar refGene "$db_dir"
+annovar/annotate_variation.pl -downdb -buildver hg19 -webfrom annovar cosmic68 "$db_dir"
+annovar/annotate_variation.pl -downdb -buildver hg19 -webfrom annovar clinvar_20150629 "$db_dir"
+annovar/annotate_variation.pl -downdb -buildver hg19 -webfrom annovar 1000g2015aug "$db_dir"
+
 
 # $ tree bin
 # bin
