43adab0c29352795ad3ca50ad2f58a9eb71dff0b
Stephen Kelly <stevekm@users.noreply.github.com>
2017-05-26 14:43:59 -0400

    add git branch checking and rearrange script

--- pipeline_functions.py
+++ pipeline_functions.py
@@ -330,3 +330,48 @@ def backup_file(input_file):
         mkdirs(os.path.dirname(new_filename))
         print('Backing up old file:\n{0}\n\nTo new location:\n{1}\n\n'.format(input_file, new_filename))
         os.rename(input_file, new_filename)
+
+
+def parse_git(attribute):
+    '''
+    Check the current git repo for one of the following items
+    attribute = "hash"
+    attribute = "hash_short"
+    attribute = "branch"
+    '''
+    import sys
+    import subprocess
+    command = None
+    if attribute == "hash":
+        command = ['git', 'rev-parse', 'HEAD']
+    elif attribute == "hash_short":
+        command = ['git', 'rev-parse', '--short', 'HEAD']
+    elif attribute == "branch":
+        command = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
+    if command != None:
+        try:
+            return(subprocess.check_output(command).strip()) # python 2.7+
+        except subprocess.CalledProcessError:
+            print('\nERROR: Git branch is not configured. Exiting script...\n')
+            sys.exit()
+
+def print_iter(iterable):
+    '''
+    basic printing of every item in an iterable object
+    '''
+    for item in iterable: print(item)
+
+def validate_git_branch(allowed = ('master', 'production')):
+    import sys
+    import subprocess
+    try:
+        current_branch = parse_git(attribute = "branch")
+        if current_branch not in allowed:
+            print("ERROR: current git branch is not allowed! Branch is: {0}.".format(current_branch))
+            print("Allowed branches are:")
+            print_iter(allowed)
+            print("Exiting...")
+            sys.exit()
+    except subprocess.CalledProcessError:
+        print('\nERROR: Git branch is not configured. Exiting script...\n')
+        sys.exit()
