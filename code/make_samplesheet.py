89464467adb0f33009a960852566bbe9122dce25
Stephen Kelly <stevekm@users.noreply.github.com>
2017-06-12 16:15:24 -0400

    refactor make samplesheet script for importing

--- code/make_samplesheet.py
+++ code/make_samplesheet.py
@@ -26,32 +26,14 @@ foo1	foo2
 import os
 import argparse
 import pipeline_functions as pl
+import global_settings
 
 # ~~~~ CUSTOM FUNCTIONS ~~~~~~ #
 
-# ~~~~ DEFAULT SETTINGS ~~~~~~ #
-samplesheet_dir = "samplesheets"
-
-# ~~~~ GET SCRIPT ARGS ~~~~~~ #
-parser = argparse.ArgumentParser(description='This script will set up a samplesheet for the reportIT pipeline in the "{0}" directory.'.format(samplesheet_dir))
-
-# a pair of ID's
-parser.add_argument("-p", dest = 'pair', action='append', metavar = 'pair of IDs', help="Paired analysis IDs to add to the sample sheet")
-
-# optional flags
-parser.add_argument("-n", "--name", default = pl.timestamp(), type = str, dest = 'name', metavar = 'sample name', help="Name to use for the samplesheet file")
-
-# the rest of the IDs
-parser.add_argument("analysis_IDs", nargs='*', help="ID's of individual analyses to add to the sample sheet")
-
-# import pdb; pdb.set_trace()
-args = parser.parse_args()
-
-analysis_IDs = args.analysis_IDs
-analysis_ID_pair = args.pair
-samplesheet_file = os.path.join(samplesheet_dir, '{0}.tsv'.format(args.name))
-
-if __name__ == "__main__":
+def main(analysis_IDs, samplesheet_file, analysis_ID_pair = None):
+    '''
+    Main control function for the program
+    '''
     print('Single IDs:')
     print(analysis_IDs)
     print('Paired IDs:')
@@ -67,3 +49,42 @@ if __name__ == "__main__":
                 print('ERROR: Number of paired analysis IDs does not equal 2! They will not be written to the samplesheet!')
     print('New samplesheet file:')
     print(samplesheet_file)
+
+def make_default_samplesheet_filepath(samplesheet_dir, name = pl.timestamp()):
+    '''
+    Make a default samplesheet file path and name
+    '''
+    samplesheet_dir = global_settings.samplesheet_dir
+    samplesheet_file = os.path.join(samplesheet_dir, '{0}.tsv'.format(name))
+    return(samplesheet_file)
+
+def run():
+    '''
+    Run the monitoring program
+    arg parsing goes here, if program was run as a script
+    '''
+    samplesheet_dir = global_settings.samplesheet_dir
+
+    # ~~~~ GET SCRIPT ARGS ~~~~~~ #
+    parser = argparse.ArgumentParser(description='This script will set up a samplesheet for the reportIT pipeline in the "{0}" directory.'.format(samplesheet_dir))
+
+    # a pair of ID's
+    parser.add_argument("-p", dest = 'pair', action='append', metavar = 'pair of IDs', help="Paired analysis IDs to add to the sample sheet")
+
+    # optional flags
+    parser.add_argument("-n", "--name", default = pl.timestamp(), type = str, dest = 'name', metavar = 'sample name', help="Name to use for the samplesheet file")
+
+    # the rest of the IDs
+    parser.add_argument("analysis_IDs", nargs='*', help="ID's of individual analyses to add to the sample sheet")
+
+    # import pdb; pdb.set_trace()
+    args = parser.parse_args()
+
+    analysis_IDs = args.analysis_IDs
+    analysis_ID_pair = args.pair
+    samplesheet_file =  make_default_samplesheet_filepath(samplesheet_dir, name = args.name)
+
+    main(analysis_IDs = analysis_IDs, analysis_ID_pair = analysis_ID_pair, samplesheet_file = samplesheet_file)
+
+if __name__ == "__main__":
+    run()
